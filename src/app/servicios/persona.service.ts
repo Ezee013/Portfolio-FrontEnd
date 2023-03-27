8521e6edd91b37d50e2b8c3b17f970659486f941
Ezequiel <ezequiel2014.edlv@gmail.com>
2023-03-26 21:29:02 -0300

    firebase deploy terminado

--- src/app/servicios/persona.service.ts
+++ src/app/servicios/persona.service.ts
@@ -2,6 +2,7 @@ import { AUTO_STYLE } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Persona } from '../model/persona.model';
 
 
@@ -12,7 +13,7 @@ import { Persona } from '../model/persona.model';
 
 export class PersonaService {
 
-  URL = "http://localhost:8080/persona/";
+  URL = environment.URL + "persona/";
 
   constructor(private httpClient: HttpClient) { }
 
